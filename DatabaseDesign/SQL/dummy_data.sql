bfd5545c2f9bb11d18fd499627ca1cb63d1ba40d
Bailey Kay <kay.b@husky.neu.edu>
2017-11-02 13:53:57 -0400

    SD103-17: Added Partners table data inserts

---
+++ DatabaseDesign/SQL/dummy_data.sql
@@ -0,0 +1,37 @@
+USE test;
+
+-- Partners Dummy Data (creates 30 entries)
+
+INSERT INTO `Partners` (`FirstName`,`LastName`,`Phone`) VALUES 
+("Jasper","Rosa","5378184316"),
+("Leslie","Mcgee","7323244320"),
+("Emerald","Reid","8178272883"),
+("Tad","Howell","4334523234"),
+("Merrill","Dillon","1488216636"),
+("Heather","Roman","9561122461"),
+("Stuart","Pittman","1826170166"),
+("Lilah","Hopper","2874875602"),
+("Cathleen","Ramirez","4382715865"),
+("Geraldine","Short","1805347741"),
+("Alea","Frederick","1196469521"),
+("Gay","Moreno","1315743885"),
+("Driscoll","Blanchard","9908705117"),
+("Brian","Crosby","6511157830"),
+("Buckminster","Wiggins","3076360684"),
+("Kitra","Medina","7225110762"),
+("Beverly","Sharpe","7702939526"),
+("Harrison","Norman","1098199403"),
+("Alexa","Martinez","5105991300"),
+("Silas","Gardner","2292369129"),
+("Nichole","Reese","7973565031"),
+("Adria","Estes","4753889493"),
+("Athena","Bentley","6585771556"),
+("Damon","Maynard","1778758790"),
+("Kay","Morgan","8437883393"),
+("Stewart","Mathews","4059830758"),
+("Lars","Warner","6207072606"),
+("Nash","Nguyen","7568201042"),
+("Delilah","Harding","2074807993"),
+("Joan","Livingston","1695312657");
+
+
