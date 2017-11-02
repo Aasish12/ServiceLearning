77b7a50d7bfba87733e38cde160fe92f6b03abad
Bailey Kay <kay.b@husky.neu.edu>
2017-11-02 14:11:12 -0400

    SD103-17: Added Locations table data insert statements

--- DatabaseDesign/SQL/dummy_data.sql
+++ DatabaseDesign/SQL/dummy_data.sql
@@ -34,4 +34,42 @@ INSERT INTO `Partners` (`FirstName`,`LastName`,`Phone`) VALUES
 ("Delilah","Harding","2074807993"),
 ("Joan","Livingston","1695312657");
 
+-- Locations Dummy Data (creates 30 entries)
+
+INSERT INTO `Locations` (`City`,`State`,`AddressLine1`,`AddressLine2`,`Country`,`Zipcode`,`isPrimary`) VALUES 
+("Jackson","Mississippi","122 Ultrices, Street","lacinia at,","Switzerland","21585",0),
+("Newark","Delaware","P.O. Box 459, 3324 Gravida. Road","ultrices posuere cubilia Curae;","Kazakhstan","40115",0),
+("Columbia","MO","Ap #255-7700 Sed Street","nunc ac","Cambodia","09744",1),
+("Fort Collins","Colorado","Ap #654-7461 Ac Av.","egestas ligula.","France","0579 VC",1),
+("Springfield","MA","Ap #781-3395 Eget St.","tortor nibh","Maldives","502364",0),
+("Richmond","Virginia","Ap #438-7275 Mi Rd.","lacus.","Botswana","28508",1),
+("Kapolei","HI","544-3280 Etiam Road","quis lectus.","Equatorial Guinea","9566",0),
+("Henderson","Nevada","P.O. Box 851, 7911 Massa. Road","libero","Moldova","924578",1),
+("Colorado Springs","Colorado","P.O. Box 329, 5445 Penatibus Av.","nulla at sem molestie","New Zealand","71369",0),
+("Knoxville","Tennessee","P.O. Box 255, 639 Eu St.","sociis natoque penatibus","Estonia","96902",0),
+("Provo","UT","P.O. Box 816, 241 Ligula. Rd.","velit in","Sri Lanka","09059",1),
+("Baltimore","MD","P.O. Box 871, 5296 Parturient Ave","Maecenas libero est,","Mozambique","115144",1),
+("Honolulu","Hawaii","P.O. Box 749, 9583 Sed St.","Maecenas libero est, congue","Montenegro","54811",1),
+("Pittsburgh","Pennsylvania","1416 Consectetuer, St.","Morbi accumsan laoreet ipsum.","Tonga","04229-973",0),
+("Hattiesburg","MS","850-995 Lorem Avenue","ligula tortor, dictum","United States","6266",0),
+("Annapolis","MD","532-9057 Porttitor Avenue","purus. Maecenas libero","Malta","16314",1),
+("Lexington","KY","Ap #757-9497 Quam Road","risus.","Bouvet Island","31377",1),
+("Orlando","FL","P.O. Box 691, 8387 Tellus Av.","Cras dolor dolor, tempus","Greece","4776",0),
+("Metairie","LA","P.O. Box 726, 9250 Feugiat. Avenue","quis lectus. Nullam suscipit,","Pakistan","8719",0),
+("Akron","OH","P.O. Box 411, 8133 Nascetur Ave","sit amet luctus vulputate,","Saint Pierre and Miquelon","04352",0),
+("Lowell","Massachusetts","Ap #331-5025 Neque. St.","euismod in, dolor. Fusce","Costa Rica","X6P 1N9",0),
+("Kansas City","KS","563-5340 Nisl. Rd.","at","Mali","87987",1),
+("Jackson","MS","Ap #142-3088 Tristique Ave","lorem,","Bonaire, Sint Eustatius","9789",1),
+("Metairie","LA","8917 Non Rd.","velit in","Comoros","73213",0),
+("Augusta","Maine","931-9652 Pharetra Av.","magna sed","Guatemala","42206",1),
+("Overland Park","Kansas","8258 Fermentum St.","Phasellus libero mauris, aliquam","Guam","5133",1),
+("Wilmington","DE","P.O. Box 435, 897 Sem Rd.","gravida sagittis. Duis","Palau","V9Z 0S0",0),
+("Knoxville","TN","857 Quam. Av.","erat, eget tincidunt dui","Iran","606788",0),
+("Boston","MA","263-1692 Consectetuer Road","iaculis","Portugal","H9I 4KN",0),
+("Racine","WI","Ap #608-8919 Pulvinar Rd.","vitae sodales nisi magna","Uruguay","43598-845",1);
+
+
+
+
+
 
