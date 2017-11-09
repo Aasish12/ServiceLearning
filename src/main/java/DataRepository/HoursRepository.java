017843fbcc760fffee8acddfbcf1e260de690502
Bailey Kay <kay.b@husky.neu.edu>
2017-11-09 11:06:26 -0500

    SD103-31: Added get all hours basic hours functionality

--- src/main/java/DataRepository/HoursRepository.java
+++ src/main/java/DataRepository/HoursRepository.java
@@ -1,10 +1,27 @@
 package DataRepository;
 
+import java.sql.SQLException;
+import java.util.List;
+
+import Data.Hours;
+import com.j256.ormlite.dao.Dao;
+import com.j256.ormlite.dao.DaoManager;
+
 /*
 * Access database from here
 * GOAL: Database function calls specifically for hours.
 *
-* Kofi Collins-Sibley
+* Bailey Kay
 * */
-public class HoursRepository {
+public class HoursRepository extends RepositoryBase {
+    private Dao<Hours, String> hoursDao;
+
+    public HoursRepository() throws SQLException {
+        super();
+        hoursDao = DaoManager.createDao(db, Hours.class);
+    }
+
+    public List<Hours> GetAllHours() throws SQLException {
+        return hoursDao.queryForAll();
+    }
 }
--- src/main/java/ServiceInterface/HoursServices.java
+++ src/main/java/ServiceInterface/HoursServices.java
@@ -1,14 +1,23 @@
 package ServiceInterface;
 
+import java.sql.SQLException;
 import java.util.*;
-import ServiceModel.Types.HoursType;
+
+import Data.Hours;
+import DataRepository.HoursRepository;
+import ServiceModel.Types.*;
 
 /*
 * Logic for courses
 *
 * Kofi Collins-Sibley
+* Bailey Kay
 * */
 public class HoursServices {
+    public List<Hours> GetAllHours() throws SQLException {
+        return new HoursRepository().GetAllHours();
+    }
+
     public ArrayList<HoursType> GetHoursByStudentId(int studentId) {
         return null;
     }
