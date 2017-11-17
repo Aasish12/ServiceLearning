0cb1e623760f61c377ccecab22d77bcf0d640eef
Collins-Sibley <kcollinssibley@redsox.com>
2017-11-16 20:00:31 -0500

    fixing typo in bash script

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,6 @@ if [[ -z ${SD103_PEM_PATH+x} ]]; then
 fi
 
 set -x
-scp -o StrictHostKeyChecking=no -i "$SD103_PEN_PATH" target/ServiceLearning.BackEnd-1.0.jar ubuntu@128.31.25.49:~/ServiceLearning.BackEnd-1.0.jar && \
-ssh -o StrictHHostKeyChecking=no -i "$SD103_PEN_PATH" ubuntu@128.31.25.49 'pkill standalone; pkill java; rm ServiceLearning.BackEnd-1.0.jar; java -jar ServiceLearning.BackEnd-1.0.jar -b'
+scp -o StrictHostKeyChecking=no -i "$SD103_PEM_PATH" target/ServiceLearning.BackEnd-1.0.jar ubuntu@128.31.25.49:~/ServiceLearning.BackEnd-1.0.jar && \
+ssh -o StrictHHostKeyChecking=no -i "$SD103_PEM_PATH" ubuntu@128.31.25.49 'pkill standalone; pkill java; rm ServiceLearning.BackEnd-1.0.jar; java -jar ServiceLearning.BackEnd-1.0.jar -b'
 set +x
