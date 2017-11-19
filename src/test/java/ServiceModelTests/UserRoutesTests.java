f57a4e842f19fd6774e207d850db2f37d4dcc2a9
Josh Peyok <peyok.j@husky.neu.edu>
2017-11-19 17:19:09 -0500

    SD103-45: Refactor directory structure to align with default maven structure.

--- src/test/java/ServiceModelTests/UserRoutesTests.java
+++ src/test/java/ServiceModelTests/UserRoutesTests.java
@@ -1,4 +1,4 @@
-package ServiceModel.Tests;
+package ServiceModelTests;
 
 import static org.hamcrest.Matchers.equalTo;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
