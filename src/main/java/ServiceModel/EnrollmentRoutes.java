ba47513a3513577bf061761b5ce6a1f7a8a07871
Aasish Basani <basani.a@husky.neu.edu>
2017-11-29 17:31:19 -0500

    fixed the mapping of course and enrollment routes

--- src/main/java/ServiceModel/EnrollmentRoutes.java
+++ src/main/java/ServiceModel/EnrollmentRoutes.java
@@ -3,64 +3,48 @@ package ServiceModel;
 import java.sql.SQLException;
 import java.util.List;
 
-import javax.ws.rs.Consumes;
-import javax.ws.rs.GET;
-import javax.ws.rs.POST;
-import javax.ws.rs.PUT;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
-import Data.Course;
+
+import javax.websocket.server.PathParam;
+
 import Data.Enrollment;
-import ServiceInterface.CourseServices;
 import ServiceInterface.EnrollmentServices;
 
 public class EnrollmentRoutes {
   private EnrollmentServices services = new EnrollmentServices();
 
   /* Get routes */
-  @GET
-  @Path("/all")
-  @Produces(MediaType.APPLICATION_JSON)
-  public List<Enrollment> getAllCourses() throws SQLException
+  @RequestMapping(value = "/enrollment/all", method = RequestMethod.GET)
+  public List<Enrollment> getAllEnrollments() throws SQLException
   { return services.getAllEnrollments(); }
 
-  @GET
-  @Path("/search/{searchText}")
-  @Produces(MediaType.APPLICATION_JSON)
-  public List<Enrollment> searchCourses(@PathParam("searchText") String searchText) throws SQLException {
+  @RequestMapping(value = "/enrollment/search/{searchText}", method = RequestMethod.GET)
+  public List<Enrollment> searchEnrollments(@PathParam("searchText") String searchText) throws SQLException {
 
     return services.SearchCourses(searchText);
   }
 
-  @GET
-  @Path("/getById/{courseId}")
-  @Produces(MediaType.APPLICATION_JSON)
-  public Enrollment getCourseById(@PathParam("courseId") int courseId) throws SQLException {
-    return services.GetById(courseId);
+  @RequestMapping(value = "/enrollment/getById/{enrollmentId}", method = RequestMethod.GET)
+  public Enrollment getEnrollmentById(@PathParam("enrollmentId") int enrollmentId) throws SQLException {
+    return services.GetById(enrollmentId);
   }
 
-  @GET
-  @Path("/getNewCourse")
-  @Produces(MediaType.APPLICATION_JSON)
-  public Enrollment getNewCourse() {
+  @RequestMapping(value = "/enrollment/getNewEnrollment", method = RequestMethod.GET)
+  public Enrollment getNewEnrollment() {
     return new Enrollment();
   }
 
   /* Post & Put */
-  @POST
-  @Path("/post")
-  @Consumes(MediaType.APPLICATION_JSON)
-  public void postNewCourse(Enrollment enrollment) throws SQLException {
+  @RequestMapping(value = "/enrollment/post", method = RequestMethod.POST)
+  public void postNewEnrollment(Enrollment enrollment) throws SQLException {
     services.PostCourse(enrollment);
   }
 
-  @PUT
-  @Path("/put")
-  @Consumes(MediaType.APPLICATION_JSON)
-  public void putCourse(Enrollment enrollment) throws SQLException {
+  @RequestMapping(value = "/enrollment/put", method = RequestMethod.PUT)
+  public void putEnrollment(Enrollment enrollment) throws SQLException {
     services.PutCourse(enrollment);
   }
 }
